b5c58376dddfd6d6ffe218711bf6a8499870d261
ivulchev <81465925+ivulchev@users.noreply.github.com>
2022-03-05 15:54:34 +0200

    stylized table

--- src/app/team.service.ts
+++ src/app/team.service.ts
@@ -26,26 +26,12 @@ export class TeamService {
       );
   }
 
-  // /** GET hero by id. Will 404 if id not found */
-  // getTeam(id: number): Observable<Team> {
-  //   const url = `${this.teamsUrl}/${id}`;
-  //   return this.http.get<Team>(url).pipe(
-  //     tap(_ => this.log(`fetched team id=${id}`)),
-  //     catchError(this.handleError<Team>(`getTeam id=${id}`))
-  //   );
-  // }
+
 
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   };
 
-  // /** PUT: update the hero on the server */
-  // updateHero(hero: Team): Observable<any> {
-  //   return this.http.put(this.teamsUrl, team, this.httpOptions).pipe(
-  //     tap(_ => this.log(`updated hero id=${team.team.id}`)),
-  //     catchError(this.handleError<any>('updateHero'))
-  //   );
-  // }
   /** Log a HeroService message with the MessageService */
   private log(message: string) {
     this.messageService.add(`TeamService: ${message}`);
