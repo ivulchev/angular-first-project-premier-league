a573eb0c05410a73bfdf5f009fdadc608c171484
ivulchev <81465925+ivulchev@users.noreply.github.com>
2022-03-05 14:19:58 +0200

    Create team.service.ts

---
+++ src/app/team.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
+
+import { Observable, of } from 'rxjs';
+
+import { Team } from './team';
+
+@Injectable({ providedIn: 'root' })
+export class TeamService {
+
+  private teamsUrl = 'api/teams';  // URL to web api
+
+  constructor(
+    private http: HttpClient) { }
+
+  /** GET teams from the server */
+  getTeams(): Observable<Team[]> {
+    return this.http.get<Team[]>(this.teamsUrl);
+  }
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+}
